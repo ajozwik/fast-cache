b558948c43b31dae61cbd77ffbc6da1b1ec17ce8
ajozwik <andrzej.jozwik@gmail.com>
2023-09-21 23:53:03 +0200

    problem with concurrent calling

--- src/main/scala/pl/jozwik/fastcache/FastCache.scala
+++ src/main/scala/pl/jozwik/fastcache/FastCache.scala
@@ -1,26 +1,22 @@
 package pl.jozwik.fastcache
 
+import com.github.benmanes.caffeine.cache.RemovalCause
 import com.github.blemale.scaffeine.{ Cache, Scaffeine }
+import com.typesafe.scalalogging.StrictLogging
 
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.ExecutionContext
 
-object FastCache {
+object FastCache extends StrictLogging {
 
-  def expireAfterWrite[K, V](duration: FiniteDuration): FastCache[K, V] = {
-    val stale = createExpireAfterWrite(duration).build[K, V]()
-    val cache = createExpireAfterWrite(duration)
-      .evictionListener { (k: K, v: V, _) =>
-        stale.put(k, v)
-      }
-      .build[K, V]()
-    new FastCache(cache, stale)
+  def createStaleCache[K, V](builder: Scaffeine[Any, Any])(implicit ex: ExecutionContext): Cache[K, V] = {
+    val staleCache = builder.build[K, V]()
+    val removeListener = (k: K, v: V, r: RemovalCause) => {
+      logger.debug(s"$k $r")
+      staleCache.put(k, v)
+    }
+    val cache      = builder.removalListener[K, V](removeListener).evictionListener(removeListener).build[K, V]()
+    val underlying = new StaleCache[K, V](cache.underlying, staleCache.underlying)
+    new Cache(underlying)
   }
 
-  private def createExpireAfterWrite(duration: FiniteDuration): Scaffeine[Any, Any] =
-    Scaffeine().expireAfterWrite(duration)
-
-}
-
-class FastCache[K, V](underlying: Cache[K, V], stale: Cache[K, V]) {
-  underlying
 }
--- src/test/scala/pl/jozwik/fastcache/FastCacheSpec.scala
+++ src/test/scala/pl/jozwik/fastcache/FastCacheSpec.scala
@@ -1,15 +1,40 @@
 package pl.jozwik.fastcache
 
 import com.github.blemale.scaffeine.Scaffeine
-import scala.concurrent.duration.*
+
+import concurrent.ExecutionContext.Implicits.global
+import concurrent.duration.*
+import scala.util.Random
 
 class FastCacheSpec extends AbstractSpec {
 
   "Fast cache " should {
-    "Clone" in {
-      val cache    = FastCache.expireAfterWrite[Int, Long](2.minute)
-      val newCache = cache
-
+    "Return empty" in {
+      val builder = Scaffeine()
+      val cache   = FastCache.createStaleCache[Long, Long](builder)
+      cache.getIfPresent(4) shouldBe empty
+    }
+    "Return from stale cache" in {
+      val builder = Scaffeine().expireAfterWrite(1.minute)
+      val cache   = FastCache.createStaleCache[String, String](builder)
+      val k       = Random.nextPrintableChar().toString
+      val v       = Random.nextPrintableChar().toString
+      cache.put(k, v)
+      val underlyingCache = cache.underlying.asInstanceOf[StaleCache[String, String]]
+      logger.debug(s"invalidateAll $k $v")
+      cache.invalidateAll()
+      cache.cleanUp()
+      underlyingCache.cache.getIfPresent(k) shouldBe null
+      cache.getIfPresent(k) shouldBe Option(v)
+      val kk = Random.nextPrintableChar().toString
+      cache.get(kk, identity)
+      underlyingCache.cache.getIfPresent(kk) shouldBe kk
+      underlyingCache.staleCache.getIfPresent(kk) shouldBe null
+      logger.debug(s"invalidate $kk")
+      cache.invalidate(kk)
+      cache.cleanUp()
+      underlyingCache.cache.getIfPresent(kk) shouldBe null
+      cache.getIfPresent(kk) shouldBe Option(kk)
     }
   }
 
